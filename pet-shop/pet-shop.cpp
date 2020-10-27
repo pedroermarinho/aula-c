14af2842a69da753f06091503a414e3cc460e7f4
Pedro Marinho <pedro.marinho238@gmail.com>
2020-10-26 22:01:26 -0400

    Update pet-shop.cpp

--- pet-shop/pet-shop.cpp
+++ pet-shop/pet-shop.cpp
@@ -30,16 +30,26 @@ void limpar_tela();
 void pausar();
 
 void menu();
+void menu_delete(client_t client_vet[], int tam);
+
+void gerar_dados(client_t client_vet[], int tam);
+void pesquisar(client_t client_vet[], int tam);
 
 void create_client(client_t client_vet[], int tam);
+void create_client(client_t *client);
+void read_client(client_t client);
 void read_client(client_t client_vet[], int tam);
 void update_client(client_t client_vet[], int tam);
 void delete_client(client_t client_vet[], int tam);
 
 void create_animal(animal_t animal_vet[], int tam);
+void create_animal(animal_t *animal);
 void read_animal(animal_t animal_vet[], int tam);
+void read_animal_simples(animal_t animal_vet[], int tam);
 void update_animal(animal_t animal_vet[], int tam);
+void delete_animal(client_t client_vet[], int tam);
 void delete_animal(animal_t animal_vet[], int tam);
+
 
 int main(void)
 {
@@ -69,6 +79,7 @@ void menu()
         printf("2 - Pesquisar\n");
         printf("3 - Deletar\n");
         printf("4 - Alterar\n");
+        printf("5 - Gerar Dados\n");
         printf("0 - Sair\n");
 
         bool isValid;
@@ -84,27 +95,32 @@ void menu()
             case 1:
                 limpar_tela();
                 create_client(client_vet, MAX_PESSOA);
-                pausar();
                 break;
 
             //Pesquisar
             case 2:
                 limpar_tela();
-                read_client(client_vet, MAX_PESSOA);
+                pesquisar(client_vet, MAX_PESSOA);
                 pausar();
                 break;
 
             //Deletar
             case 3:
                 limpar_tela();
-                delete_client(client_vet, MAX_PESSOA);
-                pausar();
+                menu_delete(client_vet, MAX_PESSOA);
                 break;
 
             //Alterar
             case 4:
                 limpar_tela();
                 update_client(client_vet, MAX_PESSOA);
+                pausar();
+                break;
+
+            //Gerar Dados
+            case 5:
+                limpar_tela();
+                gerar_dados(client_vet, MAX_PESSOA);
                 pausar();
                 break;
 
@@ -123,36 +139,101 @@ void menu()
 
     } while (true);
 }
-void menu_alterar_client(client_t client_vet[], int tam)
+
+void menu_delete(client_t client_vet[], int tam)
 {
+    system("title Menu Deletar");
+    bool continuar = true;
+    do
+    {
+        limpar_tela();
+        int op = 0;
+        system("title Pet Shop Manager");
+        printf("1 - Cliente\n");
+        printf("2 - Animais\n");
+        printf("0 - Sair\n");
+
+        bool isValid;
+        do
+        {
+            printf(">");
+            scanf("%d", &op);
+
+            isValid = true;
+            switch (op)
+            {
+            //Cliente
+            case 1:
+                limpar_tela();
+                delete_client(client_vet, MAX_PESSOA);
+                break;
+
+            //Animais
+            case 2:
+                limpar_tela();
+                delete_animal(client_vet, MAX_PESSOA);
+                pausar();
+                break;
+
+            //Sair
+            case 0:
+                limpar_tela();
+                continuar = false;
+                break;
+
+            default:
+                isValid = false;
+                break;
+            }
+        } while (!isValid);
+    } while (continuar);
 }
-void menu_alterar_animal(animal_t animal_vet[], int tam)
+void gerar_dados(client_t client_vet[], int tam)
 {
+    for (int i = 0; i < tam; i++)
+    {
+        sprintf(client_vet[i].nome, "Clinte %d", i);
+        sprintf(client_vet[i].telefone, "(11) 1111 111%d", i);
+        client_vet[i].exist = true;
+        for (int j = 0; j < MAX_ANIMAL; j++)
+        {
+            sprintf(client_vet[i].animais[j].nome, "Animal %d", j);
+            sprintf(client_vet[i].animais[j].raca, "Raça %d", j);
+            client_vet[i].animais[j].peso = j;
+            client_vet[i].animais[j].exist = true;
+        }
+    }
+    printf("\nDados Gerados\n");
 }
 
-void menu_deletar_client(client_t client_vet[], int tam)
+void pesquisar(client_t client_vet[], int tam)
 {
+    system("title Pesquisa");
+    read_client(client_vet, tam);
+
+    int op;
+    printf("\nEscolha um cliente para ver os dados:\n");
+
+    do
+    {
+        printf(">");
+        scanf("%d", &op);
+    } while (!(op >= 0 && op < tam));
+
+    if (client_vet[op].exist) // if (true ou false); (1==2) ==> valor => true ou false || 0 - false & 1 - true
+    {
+        limpar_tela();
+        read_client(client_vet[op]);
+    }
+    else
+    {
+        printf("\nCliente não existe");
+    }
 }
 
-void menu_deletar_animal(animal_t animal_vet[], int tam)
+void create_client(client_t *client)
 {
-}
-
-/*
-ter duas ou mais funções/procedimento com o mesmo nome, mas com parametros diferentes
-
-identificador = nome+parametros 
-
-imprimir(){}
-imprimir(int result){}
-imprimir(char result){}
-
-imprimir(); != imprimir(1);
-
-*/
-
-void _create_client(client_t *client)
-{
+    system("title Cadastro de Clientes");
     printf("Nome:\n");
     printf(">");
     fflush(stdin);
@@ -161,18 +242,18 @@ void _create_client(client_t *client)
     printf(">");
     fflush(stdin);
     gets(client->telefone);
+    create_animal(client->animais, MAX_ANIMAL);
     client->exist = true;
 }
 
 void create_client(client_t client_vet[], int tam)
 {
-    int i;
     int op = 0;
-    for (i = 0; i < tam; i++)
+    for (int i = 0; i < tam; i++)
     {
         if (i == 0)
         {
-            _create_client(&client_vet[i]);
+            create_client(&client_vet[i]);
         }
         else
         {
@@ -182,7 +263,7 @@ void create_client(client_t client_vet[], int tam)
             limpar_tela();
             if (op)
             {
-                _create_client(&client_vet[i]);
+                create_client(&client_vet[i]);
             }
             else
             {
@@ -193,22 +274,29 @@ void create_client(client_t client_vet[], int tam)
     }
 }
 
+void read_client(client_t client)
+{
+    if (client.exist)
+    {
+        printf("\nNome:%s", client.nome);
+        printf("\nTelefone:%s", client.telefone);
+        printf("\n\n--Dados dos animais--");
+        read_animal(client.animais, MAX_ANIMAL);
+    }
+}
 void read_client(client_t client_vet[], int tam)
 {
-    int i;
-    for (i = 0; i < tam; i++)
+    for (int i = 0; i < tam; i++)
     {
         if (client_vet[i].exist)
         {
-            printf("\n-----------%d-----------", i);
-            printf("\nNome:%s", client_vet[i].nome);
-            printf("\nTelefone:%s", client_vet[i].telefone);
+            printf("\n%d - Nome:%s", i, client_vet[i].nome);
         }
-        
     }
 }
 void update_client(client_t client_vet[], int tam)
 {
+    system("title Atualizar de Cadastros");
     read_client(client_vet, tam);
 
     int op;
@@ -218,14 +306,11 @@ void update_client(client_t client_vet[], int tam)
     {
         printf(">");
         scanf("%d", &op);
-
     } while (!(op >= 0 && op < tam));
 
     if (client_vet[op].exist) // if (true ou false); (1==2) ==> valor => true ou false || 0 - false & 1 - true
     {
-
-        _create_client(&client_vet[op]);
-
+        create_client(&client_vet[op]);
     }
     else
     {
@@ -234,6 +319,7 @@ void update_client(client_t client_vet[], int tam)
 }
 void delete_client(client_t client_vet[], int tam)
 {
+    system("title Deleta de Clientes");
     read_client(client_vet, tam);
 
     int op;
@@ -257,15 +343,150 @@ void delete_client(client_t client_vet[], int tam)
     }
 }
 
+void create_animal(animal_t *animal)
+{
+
+    system("title Cadastro de Animais");
+    printf("Nome:\n");
+    printf(">");
+    fflush(stdin);
+    gets(animal->nome);
+    printf("Raça:\n");
+    printf(">");
+    fflush(stdin);
+    gets(animal->raca);
+    printf("Peso:\n");
+    printf(">");
+    fflush(stdin);
+    scanf("%f", &animal->peso);
+    animal->exist = true;
+}
+
 void create_animal(animal_t animal_vet[], int tam)
 {
+    limpar_tela();
+    int op = 0;
+    for (int i = 0; i < tam; i++)
+    {
+        if (i == 0)
+        {
+            create_animal(&animal_vet[i]);
+        }
+        else
+        {
+            printf("\nDeseja cadastrar mais um animal? 1->sim, 0->nao\n");
+            printf(">");
+            scanf("%d", &op);
+            limpar_tela();
+            if (op)
+            {
+                create_animal(&animal_vet[i]);
+            }
+            else
+            {
+                animal_vet[i].exist = false;
+                break;
+            }
+        }
+    }
 }
+
 void read_animal(animal_t animal_vet[], int tam)
 {
+    for (int i = 0; i < tam; i++)
+    {
+        if (animal_vet[i].exist)
+        {
+            printf("\n-----------%d-----------", i);
+            printf("\nNome:%s", animal_vet[i].nome);
+            printf("\nRaça:%s", animal_vet[i].raca);
+            printf("\nPeso:%.2f", animal_vet[i].peso);
+        }
+    }
+}
+void read_animal_simples(animal_t animal_vet[], int tam)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        if (animal_vet[i].exist)
+        {
+            printf("\n%d - Nome:%s", i, animal_vet[i].nome);
+        }
+    }
 }
 void update_animal(animal_t animal_vet[], int tam)
 {
+    system("title Atualizar de Animais");
+    read_animal_simples(animal_vet, tam);
+
+    int op;
+    printf("\nEscolha um animal para ser deletado:\n");
+
+    do
+    {
+        printf(">");
+        scanf("%d", &op);
+
+    } while (!(op >= 0 && op < tam));
+
+    if (animal_vet[op].exist)
+    {
+        animal_vet[op].exist = false;
+        printf("\nAnimal %s deletado\n", animal_vet[op].nome);
+    }
+    else
+    {
+        printf("\nAnimal não existe");
+    }
 }
-void delete_animal(animal_t animal_vet[], int tam)
+void delete_animal(client_t client_vet[], int tam)
 {
+
+    system("title Deleta de Animais");
+
+    read_client(client_vet, tam);
+
+    int op;
+    printf("\nEscolha um cliente para ser deletado:\n");
+
+    do
+    {
+        printf(">");
+        scanf("%d", &op);
+
+    } while (!(op >= 0 && op < tam));
+
+    if (client_vet[op].exist)
+    {
+        delete_animal(client_vet[op].animais, MAX_ANIMAL);
+    }
+    else
+    {
+        printf("\nCliente não existe");
+    }
+}
+void delete_animal(animal_t animal_vet[], int tam){
+    
+
+        read_animal_simples(animal_vet, tam);
+
+        int op;
+        printf("\nEscolha um animal para ser deletado:\n");
+
+        do
+        {
+            printf(">");
+            scanf("%d", &op);
+
+        } while (!(op >= 0 && op < MAX_ANIMAL));
+
+        if (animal_vet[op].exist)
+        {
+            animal_vet[op].exist = false;
+            printf("\nAnimal %s deletado\n", animal_vet[op].nome);
+        }
+        else
+        {
+            printf("\nAnimal não existe");
+        }
 }
